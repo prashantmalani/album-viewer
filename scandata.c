1b3c84b067a01ca60d9ecf937aa7c28be88384d2
Prashant Malani <p.malani@gmail.com>
2015-07-22 21:57:16 -0700

    Fix bugs in IDCT and zigzag logic.

--- scandata.c
+++ scandata.c
@@ -10,9 +10,10 @@ static int one_base[] = {
 static int dezigzag_index[] = {
 	0, 1, 8, 16, 9, 2, 3, 10, 17, 24, 32, 25, 18, 11, 4, 5, 12,
 	19, 26, 33, 40, 48, 41, 34, 27, 20, 13, 6, 7, 14, 21, 28, 35,
-	42, 49, 56, 57, 50, 43, 67, 29, 22, 15, 23, 30, 37, 44, 51,
+	42, 49, 56, 57, 50, 43, 36, 29, 22, 15, 23, 30, 37, 44, 51,
 	58, 59, 52, 45, 38, 31, 39, 46, 53, 60, 61, 54, 47, 55, 62,
 	63 };
+
 static int dc_val;
 
 static int getDcVal(jfif_huff *huff)
@@ -60,7 +61,7 @@ static void dequantizeBlock(jfif_dqt *dqt, int block[])
 
 static inline float coeffX(int x)
 {
-	return (x ? 0.707 : 1);
+	return (!x ? 0.707 : 1);
 }
 
 void doIdct(int block[])
@@ -138,6 +139,5 @@ void parseScanData(uint8_t *ptr, jfif_info *j_info)
 	LOGD("Parsing Cr component\n");
 	parseComponent(j_info, block_cr, 1);
 
-	return;
 	return;
 }
